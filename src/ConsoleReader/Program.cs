e450ec4a348ebb456c7285b8b048c183d27bd879
Zach T <xslx_tnt@yahoo.com>
2021-03-23 17:36:50 -0400

    Cleaned up ConsoleReader

--- src/ConsoleReader/Program.cs
+++ src/ConsoleReader/Program.cs
@@ -9,14 +9,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Runtime;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
-using Unreal.Core;
-using Unreal.Core.Models;
 using Unreal.Core.Models.Enums;
-using Unreal.Encryption;
 
 namespace ConsoleReader
 {
@@ -27,41 +21,12 @@ namespace ConsoleReader
             var serviceCollection = new ServiceCollection()
                 .AddLogging(loggingBuilder => loggingBuilder
                     .AddConsole()
-                    .SetMinimumLevel(LogLevel.Warning));
+                    .SetMinimumLevel(LogLevel.Error));
             var provider = serviceCollection.BuildServiceProvider();
             var logger = provider.GetService<ILogger<Program>>();
 
-            //var localAppDataFolder = GetFolderPath(SpecialFolder.LocalApplicationData);
-            ////var replayFilesFolder = Path.Combine(localAppDataFolder, @"FortniteGame\Saved\Demos");
-            //var replayFilesFolder = @"D:\Projects\FortniteReplayCollection";
-            //var replayFiles = Directory.EnumerateFiles(replayFilesFolder, "*.replay");
-
-            //foreach (var replayFile in replayFiles)
-            //{
-            //    var reader = new ReplayReader(logger);
-            //    var replay = reader.ReadReplay(replayFile);
-            //}
-
-            //var replayFile = "Replays/season12_arena.replay";
-            //var replayFile = "Replays/season11.31.replay
-            var replayFile = "Replays/HaakonWin2.replay"; //Used for testing
-            //var replayFile = "Replays/season11.11.replay"; //Used for testing
-            //var replayFile = "Replays/Test.replay"; //Used for testing
-            //var replayFile = "Replays/shoottest.replay"; 
-            //var replayFile = "Replays/tournament2.replay";
-            //var replayFile = "Replays/creative-season11.21.replay";
-            //var replayFile = "Replays/creative.replay";
-            //var replayFile = "Replays/UnsavedReplay-2019.12.11-02.43.14.replay";
-            //var replayFile = "Replays/UnsavedReplay-2019.12.10-17.47.46.replay";
-            //var replayFile = "Replays/UnsavedReplay-2019.12.11-01.49.22.replay";
-            //var replayFile = "Replays/123.replay";
-            //var replayFile = "Replays/WCReplay.replay";
-            //var replayFile = "Replays/00769AB3D5F45A5ED7B01553227A8A82E07CC592.replay";
-            //var replayFile = "Replays/season12_arena.replay";
-            //var replayFile = "Replays/1.replay";
-            //var replayFile = "Replays/weapons2.replay";
-            //var replayFile = "Replays/iceblocks2.replay";
-            //var replayFile = "Replays/creativeShooting.replay";
+            
+            var replayFile = "Replays/season11.11.replay"; //Used for testing
 
             Stopwatch sw = new Stopwatch();
 
@@ -72,7 +37,7 @@ namespace ConsoleReader
 
             List<double> times = new List<double>();
 
-            var reader = new ReplayReader(null, new FortniteReplaySettings
+            var reader = new ReplayReader(logger, new FortniteReplaySettings
             {
             });
 
@@ -80,49 +45,21 @@ namespace ConsoleReader
 
             foreach (string path in Directory.GetFiles("Replays"))
             {
-                for (int i = 0; i < 5; i++)
+                for (int i = 0; i < 2; i++)
                 {
+                    Console.WriteLine($"Parsing: {path}");
+
                     ++count;
 
                     sw.Restart();
-                    var replay = reader.ReadReplay(replayFile, ParseType.Full);
+                    var replay = reader.ReadReplay(path, ParseType.Full);
 
                     sw.Stop();
 
-                    var aff = replay.GameInformation.Players.OrderByDescending(x => x.Locations.Count);
-
-                    var player = replay.GameInformation.Players.FirstOrDefault(x => x.IsPlayersReplay);
-                    var aaa = replay.GameInformation.Players.SelectMany(y => y.Shots);
-
                     Console.WriteLine($"Elapsed: {sw.ElapsedMilliseconds}ms. Total Groups Read: {reader?.TotalGroupsRead}. Failed Bunches: {reader?.TotalFailedBunches}. Failed Replicator: {reader?.TotalFailedReplicatorReceives} Null Exports: {reader?.NullHandles} Property Errors: {reader?.PropertyError} Failed Property Reads: {reader?.FailedToRead}");
-
-                    var killFeed = replay.GameInformation.KillFeed.Where(x => x.FinisherOrDowner?.IsPlayersReplay == true).ToList();
-
-                    var bots = replay.GameInformation.Players.Where(x => !String.IsNullOrEmpty(x.BotId)).ToList();
-                    var nonBots = replay.GameInformation.Players.Where(x => !x.IsBot).ToList();
-                    var asdfa = replay.GameInformation.Teams.OrderByDescending(x => x.Players.Count);
-
-                    //Console.WriteLine($"\t - Properties Read: {reader.TotalPropertiesRead}");
-
-                    //Console.WriteLine($"Elapsed: {sw.ElapsedMilliseconds}ms. Total Llamas: {reader.GameInformation.Llamas.Count}. Unknown Fields: {NetFieldParser.UnknownNetFields.Count}");
 
                     totalTime += sw.Elapsed.TotalMilliseconds;
                     times.Add(sw.Elapsed.TotalMilliseconds);
-
-                    /*
-                    foreach (Llama llama in replay.GameInformation.Llamas)
-                    {
-                        //Console.WriteLine($"\t -{llama}");
-                    }
-
-
-                    var c = replay.GameInformation.Players.Where(x => x.IsPlayersReplay);
-                    var d = replay.GameInformation.Teams.OrderByDescending(x => x.Players.Count);
-                    //var a = NetFieldParser.UnknownNetFields;
-                    var kills = d.FirstOrDefault().Players.Sum(x => x.TotalKills);
-
-                    var asdfas = replay.GameInformation.Teams.OrderByDescending(x => x.Players.Count);
-                    var totalBots = replay.GameInformation.Players.Count(x => x.IsBot);*/
                 }
             }
 
