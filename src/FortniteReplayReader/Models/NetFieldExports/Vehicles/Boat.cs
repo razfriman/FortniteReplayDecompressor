d23065ac933d2df1cb472d321951fad89616f08a
Zach T <xslx_tnt@yahoo.com>
2021-04-02 22:25:17 -0400

    Fixed boat export

--- src/FortniteReplayReader/Models/NetFieldExports/Vehicles/Boat.cs
+++ src/FortniteReplayReader/Models/NetFieldExports/Vehicles/Boat.cs
@@ -8,9 +8,8 @@ using Unreal.Core.Models.Enums;
 
 namespace FortniteReplayReader.Models.NetFieldExports.Vehicles
 {
-    //??
     [NetFieldExportGroup("/Game/Athena/DrivableVehicles/Meatball/Meatball_Large/MeatballVehicle_L.MeatballVehicle_L_C", ParseType.Debug)]
-    public class LootShark : BaseVehicle
+    public class Boat : BaseVehicle
     {
     }
 }
