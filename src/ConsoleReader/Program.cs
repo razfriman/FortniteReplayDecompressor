1fea4188443fd1a02897c568ffd35dadb81d10c1
Raz Friman <raz@canva.com>
2021-06-16 08:13:11 +1000

    Simple

--- src/ConsoleReader/Program.cs
+++ src/ConsoleReader/Program.cs
@@ -7,28 +7,23 @@ using Unreal.Core.Models.Enums;
 
 namespace ConsoleReader
 {
-    unsafe class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            var serviceCollection = new ServiceCollection()
+            var logger = new ServiceCollection()
                 .AddLogging(loggingBuilder => loggingBuilder
                     .AddConsole()
-                    .SetMinimumLevel(LogLevel.Information));
-            var provider = serviceCollection.BuildServiceProvider();
-            var logger = provider.GetService<ILogger<Program>>();
-            var replayFile = "Replays/newSeason.replay"; 
+                    .SetMinimumLevel(LogLevel.Information))
+                .BuildServiceProvider()
+                .GetService<ILogger<Program>>();
             var reader = new ReplayReader(logger, new FortniteReplaySettings
             {
                 PlayerLocationType = LocationTypes.All,
             });
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine(replayFile);
-                var replay = reader.ReadReplay(replayFile, ParseType.Full);
-            }
-
-            Console.WriteLine();
+            
+            var replayFile = "Replays/newSeason.replay";
+            var replay = reader.ReadReplay(replayFile, ParseType.Full);
             Console.WriteLine("---- done ----");
         }
     }
